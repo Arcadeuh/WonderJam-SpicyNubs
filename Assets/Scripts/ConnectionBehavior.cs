bb202c35ac2fea89861587f19a8729f09c2f7211
Arcadeuh <fouque.bastien@outlook.fr>
2021-10-10 10:50:46 -0400

    Tru to fix boxer

--- Assets/Scripts/ConnectionBehavior.cs
+++ Assets/Scripts/ConnectionBehavior.cs
@@ -59,6 +59,7 @@ public class ConnectionBehavior: MonoBehaviour
     {
         currentConnection -= damage;
         connectionBar.SetConnection(currentConnection);
+        GetComponent<PlayerCombatScript>().GetHit();
     }
     public void Connection(int power)
     {
